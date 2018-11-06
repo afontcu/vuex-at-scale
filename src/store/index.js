b0a0fce274b5c0a3739dc3c750861754bd51f08c
Adrià Fontcuberta <adria@calidae.com>
2018-11-06 09:12:10 +0100

    Add basic store structure

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,12 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
+import postsModule from './modules/posts'
+
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  modules: {
+    postsModule,
+  },
 })
